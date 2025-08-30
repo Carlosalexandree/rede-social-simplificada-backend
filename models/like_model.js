46d93a783105e493f7d7a00b89db9965926ec483
Bowie˜ <n.eco.12354@gmail.com>
2025-08-30 19:25:29 -0300

    add like_model

---
+++ models/like_model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const likeSchema = mongoose.Schema({
+  post_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  post_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Post",
+    required: true,
+  },
+  comment_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Comment",
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Like = mongoose.model("Like", likeSchema);
+
+export default Like;
