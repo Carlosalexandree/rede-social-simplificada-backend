7b4ba54a3d622cbc4f70693e079accf46cdf9b19
Bowie˜ <n.eco.12354@gmail.com>
2025-09-05 15:07:07 -0300

    create comment user and router

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+/* eslint-disable no-console */
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import mongoose from "mongoose";
+import userRouter from "./routes/user_routes.js";
+import commentRouter from "./routes/comment_routes.js";
 
 const app = express();
 
@@ -8,6 +12,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-const PORT = process.env.PORT || 3000;
+app.use("/users", userRouter);
+app.use("/comment", commentRouter);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose
+  .connect("mongodb://neco:123456@127.0.0.1:27017/")
+  .then(() => {
+    const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
