48d50a1a6e74c6df2e2bd130b36b50f0103ac0e7
Bowie˜ <n.eco.12354@gmail.com>
2025-09-06 15:05:46 -0300

    update file helpers

--- shared/helpers.js
+++ shared/helpers.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line import/prefer-default-export
 export const isDataValid = (value) => {
-  return typeof value === "string" && value && value.trim();
+  return typeof value === "string" && value && value.trim().length > 0;
 };
